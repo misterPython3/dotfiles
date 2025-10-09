534e5f14fee9614436561f997a7bd9584f3dae07
misterPython3 <genesismatic03@gmail.com>
2025-10-10 03:24:01 +0800

    i3 script update

--- i3/scripts/random_wallpaper.sh
+++ i3/scripts/random_wallpaper.sh
@@ -1,31 +1,21 @@
 #!/usr/bin/bash
 
-current_wallpaper_path=~/.config/i3/scripts/current_wallpaper
-directory=$1
-if [[ ! -d $directory ]];then
-    if [[ -a $directory ]];then
-	output="'$directory' is not a directory"
-    else
-	output="'$directory' does not exist"
-    fi
-    echo "$output, will use current directory"
-    directory=$(realpath ./)
+if [[ -d $1 ]];then
+    WALLPAPER_DIRECTORY=$1
+else
+    WALLPAPER_DIRECTORY=~/Downloads
+fi
+if [[ -f $2 ]];then
+    CURRENT_WALLPAPER_SYM=$2
+else 
+    CURRENT_WALLPAPER_SYM=~/.local/share/wallpaper/current_wallpaper
+fi
+CURWAL_SYM_DIR=$(dirname $CURRENT_WALLPAPER_SYM)
+if [[ ! -d $CURWAL_SYM_DIR ]];then
+    mkdir -p $CURWAL_SYM_DIR
 fi
 
-images=$(ls -1 $directory | grep -e ".*\.\(jpe\?\|pn\)g$")
-count=$(printf "$images" | wc --lines)
-if [[ $count -le 0 ]];then
-    echo "No image found in this directory."
-    exit 1
-fi
-current_wallpaper=$(cat $current_wallpaper_path)
-
-if [[ -f $current_wallpaper_path && $current_wallpaper =~ $directory && $count -gt 1 ]];then
-    filename=$(printf "$current_wallpaper" | sed s/.*\\/// )
-    images=$(printf "$images" | sed "/^$filename$/d")
-    count=$((count - 1))
-fi
-wallpaper=$directory/$(printf "$images" | sed -n "$(shuf -i 1-$count -n 1)p")
-
-echo "$wallpaper" | tee -p $current_wallpaper_path 1>/dev/null 2>&1
-xwallpaper --tile "$wallpaper"
+CURRENT_WALLPAPER=$(readlink -e "$CURRENT_WALLPAPER_SYM")
+WALLPAPER=$(find "$WALLPAPER_DIRECTORY" -maxdepth 1 -type f -regex ".*\.\(pn\|jpe?\)g$" | sed "\%${CURRENT_WALLPAPER:-\\n}%d" | shuf | head -n 1)
+ln -sf "$WALLPAPER" "$CURRENT_WALLPAPER_SYM"
+xwallpaper --tile "$WALLPAPER"
